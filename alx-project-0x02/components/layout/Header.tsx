144e4bae43f88eea0e5344f7627ea31b9da206c8
Dydex <supremeajala@gmail.com>
2025-09-23 14:43:11 +0100

    first commit for task 2

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
     <header>
+      <nav>
+        <li className='hover:underline'>
+          <Link href='/about'>About</Link>
+        </li>
+
+        <li className='hover:underline'>
+          <Link href='/home'>Home</Link>
+        </li>
+      </nav>
       <h6>This is the header</h6>
     </header>
   );
---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,5 @@
+const AboutPage: React.FC = () => {
+  return <h1>This is the About Page</h1>;
+};
+
+export default AboutPage;
